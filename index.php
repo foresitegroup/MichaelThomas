0e7ebef464c1641a03fcfaebf04075bf01697c4b
foresitegroup <mark@foresitegrp.com>
2015-02-09 17:57:32 -0600

    A test

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ include "header.php";
 ?>
 
 Our company culture is directly responsible for our successful performance. We are a national executive search firm with a national candidate network and deep industry knowledge to help you, the client, achieve your business goals and success. We believe your executive recruiting partner should understand your goals and deliver upon your requirements.
+<!-- I made this change at home -->
 
 <?php include "footer.php"; ?>
