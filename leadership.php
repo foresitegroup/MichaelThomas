c902e2fe939fd9d998ffba12092e9bd935ba812f
Mark Lippert <mark@foresitegrp.com>
2015-03-24 10:06:40 -0500

    Add text

--- leadership.php
+++ leadership.php
@@ -16,14 +16,14 @@ Michael W Collentine. Managing Partner<br>
 Over the past 15 years, Michael has led the completion of over 250 middle to senior-level searches representing Banking and Financial Services, State and Private Pensions Funds, Consumer &amp; Building Products, Industrial, Chemical &amp; Pharmaceutical Manufacturing, OEM, Accounting &amp; Finance, Energy, Oil and Gas, Construction, Legal &amp; Private Equity.<br>
 <br>
 
-Prior to forming Michael-Thomas Executive Search, Michael has held a senior leadership role as Executive Senior Partner of the executive search firm The Lucas Group a national search firm for 10 years. Michael was also responsible for significant growth and executive leadership as President of Capital Sporting Goods, Inc and the CEO of Reeder &amp; McGaughey.<br>
+Prior to forming Michael-Thomas Executive Search, Michael has held a senior leadership role as Executive Senior Partner of the executive search firm The Lucas Group a national search firm for 10 years. Michael was also responsible for significant growth and executive leadership as President of Capital Sporting Goods, Inc and the CEO of Reeder &amp; McGaughey.  And as Vice President of Sales and Marketing of Pivalco Inc., Michael represented many blue chip manufacturers for industrial process equipment and services.<br>
 <br>
 
 Michael earned a Bachelor degree from St. Norbert College in De Pere, Wisconsin, majoring in History and Economics. He also is a graduate of Wentworth Military Academy.<br>
 <br>
 
-<!-- Principal Contact:<br>
-Michael W Collentine. Managing Partner<br> -->
+<strong>Principal Contact</strong><br>
+Michael W Collentine. Managing Partner<br>
 Email: <?php email("mcollentine@mtrecruits.com"); ?>
 
 <?php include "footer.php"; ?>
