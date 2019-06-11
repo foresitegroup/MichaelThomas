47a1057741a1a464ebe15493ee79e4808397c042
foresitegroup <mark@foresitegrp.com>
2019-06-11 09:32:21 -0500

    Add companies

--- industries-served.php
+++ industries-served.php
@@ -10,7 +10,7 @@ include "header.php";
 Michael-Thomas Executive Search focuses on working closely with companies in recruiting outstanding candidates for a variety of mid to senior level positions. We are proud of the hard and close work we have performed for leading national and international companies and built lasting relationships with our clients.
 </article>
 
-<h2 style="text-align: center;">Top Ten Companies</h2><br>
+<h2 style="text-align: center;">Top Companies</h2><br>
 <article class="one-col logos">
 <img src="images/logo-koch.png" alt="Koch Industries">
 <img src="images/logo-etrade.png" alt="E*Trade">
@@ -21,7 +21,9 @@ Michael-Thomas Executive Search focuses on working closely with companies in rec
 <img src="images/logo-jpmorgan.png" alt="JPMorgan Securities">
 <img src="images/logo-charles-schwab.png" alt="Charles Schwab">
 <img src="images/logo-tyco.png" alt="Tyco International">
+<img src="images/logo-j-streicher.png" alt="J. Streicher">
 <img src="images/logo-morganstanley.png" alt="Morgan Stanley-Smith Barney">
+<img src="images/logo-saint-gobain.png" alt="Saint-Gobain">
 </article>
 
 <div class="grayband">
